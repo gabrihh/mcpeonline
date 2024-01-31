49f71ff6696558cc8e2481dcd92c849990bf0a01
gabrihh <120270743+gabrihh@users.noreply.github.com>
2024-01-31 10:49:35 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // firebase.js
 
-const { initializeApp, getAnalytics } = require("firebase/app");
-const { getFirestore, collection, addDoc } = require("firebase/firestore");
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCjWBGFYv2Jwsc28F92RyDoNs2mMEqCZAQ",
@@ -13,19 +13,21 @@ const firebaseConfig = {
   measurementId: "G-EFXG29LZ8W"
 };
 
-// Initialize Firebase
+// Inicialize o Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app); // Adicionando a inicialização do Analytics
 
 const db = getFirestore(app);
 
 async function adicionarServidor(nome, porta, versao) {
-  const docRef = await addDoc(collection(db, "servidores"), {
-    nome: nome,
-    porta: porta,
-    versao: versao,
-  });
+  try {
+    const docRef = await addDoc(collection(db, "servidores"), {
+      nome: nome,
+      porta: porta,
+      versao: versao,
+    });
 
-  console.log("Servidor adicionado com ID: ", docRef.id);
-}ID: ", docRef.id);
+    console.log("Servidor adicionado com ID: ", docRef.id);
+  } catch (error) {
+    console.error("Erro ao adicionar servidor: ", error);
+  }
 }
